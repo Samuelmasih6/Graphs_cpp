0a0d4c2d84d300613480c8f091ad4d0321d2a047
Samuel Masih <sidhusamuel567@gmail.com>
2026-08-17 10:31:39 +0530

    length of common subsequence

--- DP/LCS/19_lcs.cpp
+++ DP/LCS/19_lcs.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int LCS(string s1,string s2,int n,int m){
+    if(n==0 || m==0)
+        return 0;
+
+    if(s1[n-1]==s2[m-1])
+        return 1+LCS(s1,s2,n-1,m-1);
+    else
+        return max(LCS(s1,s2,n,m-1),LCS(s1,s2,n-1,m));
+}
+
 int main(){
+    int n,m;
+    cin>>n>>m;
     string s1,s2;
     cin>>s1>>s2;
+    cout<<"length: "<<LCS(s1,s2,n,m);
 }
