594aff3494b2e2b2556b496409e0ea16742646f1
Samuel Masih <sidhusamuel567@gmail.com>
2026-08-17 12:37:18 +0530

    LC substring, bottom-up approach

---
+++ DP/LCS/22_LCsubstringBUP.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int n = s1.length();
+    int m = s2.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                ans = max(ans, dp[i][j]);
+            }
+            else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    cout << "Length: " << ans << endl;
+
+    return 0;
+}
